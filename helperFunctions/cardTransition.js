751621067705c08f5976ad9a6c49a3981ada7b4b
Diego Vazquez <diegovazquezny@gmail.com>
2021-02-06 21:23:17 -0500

    refactored cardContainer to use useReducer instead of useState

--- helperFunctions/cardTransition.js
+++ helperFunctions/cardTransition.js
@@ -1,18 +1,9 @@
-export default function cardTransition(parentRef, childRef, setCardData, data) {
+export default function cardTransition(parentRef, childRef, dispatch, data) {
   parentRef.current.style.height = '100vh';
   parentRef.current.style.minHeight = '100%';
   setTimeout(() => {
     window.location.href = '#card';
     childRef.current.style.display = 'flex', 760;
-    setCardData(data)
+    dispatch({type: 'setCardData', payload: data})
   }, 760);
-}
-
-export function disableButton(ref) {
-  ref.current.style.pointerEvents = 'none';
-  return ref;
-}
-
-export function hideParent(ref) {
-  ref.current.style.height = '0px';
 }
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Dev Flash Cards</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
