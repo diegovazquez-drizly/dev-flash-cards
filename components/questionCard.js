43934f33c460b2c0ac35e42927bd4405224321ee
diegovazquezny <diegovazquezny@gmail.com>
2023-05-28 08:17:28 -0400

    add collapsed category cards

--- components/questionCard.js
+++ components/questionCard.js
@@ -8,7 +8,7 @@ export default function QuestionCard({card}) {
   if (!card) return null;
 
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
+    <Card shadow="sm" padding="lg" radius="md" withBorder >
       <Group position="apart" mt="md" mb="xs">
         <Text weight={500}>{card.card_question}</Text>
         <Badge color="pink" variant="light">
