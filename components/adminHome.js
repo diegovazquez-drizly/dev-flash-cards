e61853b342bacfdf521d8a2b48168002ba44af83
diegovazquezny <diegovazquezny@gmail.com>
2023-05-28 07:38:42 -0400

    buttons

--- components/adminHome.js
+++ components/adminHome.js
@@ -6,10 +6,10 @@ export default function AdminHome({setPage, setAdmin}) {
   return (
     <div className={styles.AdminHomeOptionsContainer}>
     <h2>Admin Options</h2>
-      <Button className={styles.AdminHomeOptions} onClick={() => setPage('new')}>Add question</Button>
-      <Button className={styles.AdminHomeOptions} onClick={() => setPage('edit')}>Edit question</Button>
-      <Button className={styles.AdminHomeOptions} onClick={() => setPage('all')}>All questions</Button>
-      <Button className={styles.AdminHomeOptions} onClick={() => {setAdmin(false)}}>Home</Button>
+      <Button className={styles.AdminHomeButtons} onClick={() => setPage('new')}>Add question</Button>
+      <Button className={styles.AdminHomeButtons} onClick={() => setPage('edit')}>Edit question</Button>
+      <Button className={styles.AdminHomeButtons} onClick={() => setPage('all')}>All questions</Button>
+      <Button className={styles.AdminHomeButtons} onClick={() => {setAdmin(false)}}>Home</Button>
     </div>
   )
 }
